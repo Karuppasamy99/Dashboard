e20759b9f35d3422383c6e2b8483da12ab2d283d
Karuppasamy99 <karuppasamy.a@hotmail.com>
2023-07-07 13:06:14 +0530

    doughnut chart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,24 @@
-
+import { Doughnut } from 'react-chartjs-2';
 
 const Dashboard = () => {
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow'],
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      },
+    ],
+  };
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <h2>Donut Chart</h2>
+      <Doughnut data={data} />
+    </div>
+  );
+};
+
 
 export default Dashboard
